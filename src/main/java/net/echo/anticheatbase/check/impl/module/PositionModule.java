9db781c6b29f41cad961e252b4c893b0759866e5
xEcho1337 <xreflection@outlook.it>
2024-04-30 14:15:15 +0200

    Make position values modifiable

--- src/main/java/net/echo/anticheatbase/check/impl/module/PositionModule.java
+++ src/main/java/net/echo/anticheatbase/check/impl/module/PositionModule.java
@@ -46,4 +46,100 @@ public class PositionModule extends AbstractCheck {
             }
         }
     }
+
+    public double getPosX() {
+        return posX;
+    }
+
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+
+    public double getPosY() {
+        return posY;
+    }
+
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
+
+    public double getPosZ() {
+        return posZ;
+    }
+
+    public void setPosZ(double posZ) {
+        this.posZ = posZ;
+    }
+
+    public double getLastX() {
+        return lastX;
+    }
+
+    public void setLastX(double lastX) {
+        this.lastX = lastX;
+    }
+
+    public double getLastY() {
+        return lastY;
+    }
+
+    public void setLastY(double lastY) {
+        this.lastY = lastY;
+    }
+
+    public double getLastZ() {
+        return lastZ;
+    }
+
+    public void setLastZ(double lastZ) {
+        this.lastZ = lastZ;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(float yaw) {
+        this.yaw = yaw;
+    }
+
+    public float getLastYaw() {
+        return lastYaw;
+    }
+
+    public void setLastYaw(float lastYaw) {
+        this.lastYaw = lastYaw;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch = pitch;
+    }
+
+    public float getLastPitch() {
+        return lastPitch;
+    }
+
+    public void setLastPitch(float lastPitch) {
+        this.lastPitch = lastPitch;
+    }
+
+    public boolean isClaimsOnGround() {
+        return claimsOnGround;
+    }
+
+    public void setClaimsOnGround(boolean claimsOnGround) {
+        this.claimsOnGround = claimsOnGround;
+    }
+
+    public boolean isLastClaimsOnGround() {
+        return lastClaimsOnGround;
+    }
+
+    public void setLastClaimsOnGround(boolean lastClaimsOnGround) {
+        this.lastClaimsOnGround = lastClaimsOnGround;
+    }
 }
